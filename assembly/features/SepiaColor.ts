20e352248908eb34f73b087f4837f0906bf0a8a6
bakugod <bakugod@yandex.ru>
2019-07-25 11:21:52 +0300

    Editorial fixes

--- assembly/features/SepiaColor.ts
+++ assembly/features/SepiaColor.ts
@@ -3,29 +3,6 @@ const BYTES_PER_PIXEL = 4;
 function sepiaColors(width: i32, height: i32): void {
     let offset = width * height * BYTES_PER_PIXEL;
 
-    //(112, 66, 20)
-
-    // outputRed = (inputRed * .393) + (inputGreen * .769) + (inputBlue * .189)
-    // outputGreen = (inputRed * .349) + (inputGreen * .686) + (inputBlue * .168)
-    // outputBlue = (inputRed * .272) + (inputGreen * .534) + (inputBlue * .131)
-
-//     for (let i = 0; i < offset; i += 4) {
-
-//         let red = load<u8>(i);
-//         let green = load<u8>(i + 1);
-//         let blue = load<u8>(i + 2);
-//         let alpha = load<u8>(i + 3);
-
-
-//         let oRed = (red * .393) + (green * .669) + (blue * .389) / 3;
-//         let oGreen = (red * .349) + (green * .586) + (blue * .368) / 3;
-//         let oBlue = (red * .272) + (green * .434) + (blue * .00031) / 3;
-
-//         rgbValues[i + 2] = (byte)Math.Min((.393 * red) + (.769 * green) + (.189 * blue), 255.0); // red
-//         rgbValues[i + 1] = (byte)Math.Min((.349 * red) + (.686 * green) + (.168 * blue), 255.0); // green
-//         rgbValues[i + 0] = (byte)Math.Min((.272 * red) + (.534 * green) + (.131 * blue), 255.0); // blue
-// }
-
     for (let y = 0; y < height; y++) {
         for (let x = 0; x < width; x++) {
 
