1251486ac510a1307e2f43ea1ea0f7f06c77e928
bakugod <bakugod@yandex.ru>
2019-07-25 11:22:10 +0300

    Hooked up new fetures

--- assembly/index.ts
+++ assembly/index.ts
@@ -2,12 +2,16 @@ import { invertColors } from './features/InvertColor';
 import { greyScale } from './features/GreyScale'
 import { loliColor } from './features/LoliColor'
 import { sepiaColors } from './features/SepiaColor'
+import { blueColors } from './features/BlueColors'
+import { blurFilter } from './features/BlurFilter'
+
+
 
 export {
 	invertColors,
 	greyScale,
 	loliColor,
 	sepiaColors,
+	blueColors,
+	blurFilter,
 };
-
-
