ed382dc87d36e7e46e567d149f376cff568739c4
bakugod <bakugod@yandex.ru>
2019-07-25 11:22:43 +0300

    Redo C&P code

--- wasm.loader.js
+++ wasm.loader.js
@@ -1,12 +1,29 @@
-(function (){
+(function () {
     const BYTES_PER_PIXEL = 4;
     const PAGE_SIZE = 64 * 1024;
 
-    function init({ instance, memory }){
-        const canvas = document.getElementById('canvas')
-        const ctx = canvas.getContext('2d')
-        const uploader = document.getElementById('uploader')
-        const filters = document.getElementById('filters')
+    const canvas = document.getElementById('canvas')
+    const ctx = canvas.getContext('2d')
+    const uploader = document.getElementById('uploader')
+    const filters = document.getElementById('filters')
+
+
+    const hookedFilters = (functionName, img, memory) => {
+        functionName(
+            img.width,
+            img.height
+        )
+        const resultData = new Uint8ClampedArray(
+            memory.buffer,
+            img.width * img.height * BYTES_PER_PIXEL,
+            img.width * img.height * BYTES_PER_PIXEL,
+        )
+    
+        ctx.putImageData(new ImageData(resultData, img.width, img.height), 0, 0)
+    }
+
+    function init({ instance, memory }) {
+
 
         const img = new Image()
         let imageData = null;
@@ -14,7 +31,7 @@
 
         uploader.addEventListener('change', event => {
             img.src = URL.createObjectURL(event.target.files[0]);
-    
+
             img.onload = _ => {
                 canvas.width = img.width;
                 canvas.height = img.height;
@@ -27,7 +44,7 @@
                 //because we doesn't want rewrite old bytes
                 const minimumMemorySize = bytesPerImage * 2;
 
-                if(memory.buffer.byteLength < minimumMemorySize){
+                if (memory.buffer.byteLength < minimumMemorySize) {
                     const pagesNeeded = Math.ceil(minimumMemorySize / PAGE_SIZE)
                     memory.grow(pagesNeeded)
                 }
@@ -38,85 +55,53 @@
 
         filters.addEventListener('change', event => {
             console.log(event.target.value)
-            if(img.width === 0 || img.height === 0){
+            if (img.width === 0 || img.height === 0) {
                 ctx.font = '32px serif';
                 ctx.fillText('Downoload image', 0, 50);
             }
-            else{
+            else {
                 switch (event.target.value) {
-                    case 'invert' : {
-                        instance.exports.invertColors(
-                            img.width,
-                            img.height
-                        )
-                        const resultData = new Uint8ClampedArray(
-                            memory.buffer,
-                            img.width * img.height * BYTES_PER_PIXEL,
-                            img.width * img.height * BYTES_PER_PIXEL,
-                        )
-
-                        ctx.putImageData(new ImageData(resultData,  img.width, img.height), 0, 0)
+                    case 'invert': {
+                        hookedFilters(instance.exports.invertColors, img, memory)
                         break;
                     }
-                    case 'grey' : {
-                        instance.exports.greyScale(
-                            img.width,
-                            img.height
-                        )
-                        const resultData = new Uint8ClampedArray(
-                            memory.buffer,
-                            img.width * img.height * BYTES_PER_PIXEL,
-                            img.width * img.height * BYTES_PER_PIXEL,
-                        )
-
-                        ctx.putImageData(new ImageData(resultData,  img.width, img.height), 0, 0)
+                    case 'grey': {
+                        hookedFilters(instance.exports.greyScale, img, memory)
                         break;
                     }
-                    case 'sepia' : {
-                        instance.exports.sepiaColors(
-                            img.width,
-                            img.height
-                        )
-                        const resultData = new Uint8ClampedArray(
-                            memory.buffer,
-                            img.width * img.height * BYTES_PER_PIXEL,
-                            img.width * img.height * BYTES_PER_PIXEL,
-                        )
-
-                        ctx.putImageData(new ImageData(resultData,  img.width, img.height), 0, 0)
+                    case 'sepia': {
+                        hookedFilters(instance.exports.sepiaColors, img, memory)
                         break;
                     }
-                    case 'lolicolor' : {
-                        instance.exports.loliColor(
-                            img.width,
-                            img.height
-                        )
-                        const resultData = new Uint8ClampedArray(
-                            memory.buffer,
-                            img.width * img.height * BYTES_PER_PIXEL,
-                            img.width * img.height * BYTES_PER_PIXEL,
-                        )
-
-                        ctx.putImageData(new ImageData(resultData,  img.width, img.height), 0, 0)
+                    case 'blue': {
+                        hookedFilters(instance.exports.blueColors, img, memory)
                         break;
                     }
-                    default : {
-                        ctx.putImageData(new ImageData(imageData.data,  img.width, img.height), 0, 0)
+                    case 'lolicolor': {
+                        hookedFilters(instance.exports.loliColor, img, memory)
+                        break;
+                    }
+                    case 'blur': {
+                        hookedFilters(instance.exports.blurFilter, img, memory)
+                        break;
+                    }
+                    default: {
+                        ctx.putImageData(new ImageData(imageData.data, img.width, img.height), 0, 0)
                         break;
                     }
                 }
             }
-        })    
+        })
     }
 
 
-    async function load(){
+    async function load() {
         let memory = new WebAssembly.Memory({ initial: 1 });
         const imports = {
             env: {
                 memory,
-                abort: () => { 
-                    throw Error('some troubles here') 
+                abort: () => {
+                    throw Error('some troubles here')
                 },
             }
         }
@@ -127,7 +112,7 @@
             , imports
         )
 
-        if(instance.exports.memory){
+        if (instance.exports.memory) {
             memory = instance.exports.memory;
         }
 
